6befe780d6d9da5f1cf8398920b40a5782b2425a
JEISON MARULANDA ESTRADA <jemarulanda@ulasallista.edu.co>
2019-09-11 14:24:19 -0500

    Se implementa excepciones

--- app/file.py
+++ app/file.py
@@ -10,7 +10,6 @@ class File:
     @classmethod
     def main(cls, source):   
 
-        print(source['seconds'])
         sched = BlockingScheduler()
 
         Mongo_URI = source['urimongo']#Direccion
@@ -32,48 +31,60 @@ class File:
             #Lista con todos los ficheros del directorio:
             lstDir = os.walk(path)   #os.walk()Lista directorios y ficheros
 
-            #Crea una lista de los ficheros dat que existen en el directorio y los incluye a la lista.
-            for root, dirs, files in lstDir:
-                for fichero in files:
-                    (nombreFichero, extension) = os.path.splitext(fichero)
-                    if(extension == source['extensionfile']):
-                        lstFiles.append(nombreFichero+extension)
+            try:
+                #Crea una lista de los ficheros dat que existen en el directorio y los incluye a la lista.
+                for root, dirs, files in lstDir:
+                    for fichero in files:
+                        (nombreFichero, extension) = os.path.splitext(fichero)
+                        if(extension == source['extensionfile']):
+                            lstFiles.append(nombreFichero+extension)
+            except:
+                print('Falla verificando los archivos')                 
+            
+            try:
+                #Recorrer por pares y construir el json con el tamaño de la cabecera (primera linea)
+                for j in lstFiles:
+                    nameFile=j
+                    collection = db[nameFile[17:-4]]
 
-            #Recorrer por pares y construir el json con el tamaño de la cabecera (primera linea)
-            for j in lstFiles:
-                nameFile=j
-                collection = db[nameFile[17:-4]]
+                    with open(path+nameFile, 'r') as file:
+                        
+                        try:
+                            for i, line in enumerate(file):
+                                data = {}
+                                data_split = line.split('|')  # Flat file line size
+                                if i == 0:
+                                    cabecera = line.split('|')
 
-                with open(path+nameFile, 'r') as file:
+                                if i > 0:
+                                    if len(data_split) > 0:
+                                        data['Flag'] = '0'
+                                        for x in range(len(cabecera)):
+                                            data[cabecera[x].rstrip('\n')] = data_split[x].rstrip('\n')
+
+                                    collection.insert_one({"Json": f'{data}'})
+                                    
+                        except ConnectionFailure:
+                            print('Cannot connect to Momgo DB')
+                        except:
+                            print('Falla construyendo el json para la bd')
+                        file.close()
+                        print('Archivo procesado: '+nameFile)
+                        try:
+                            os.rename(path+nameFile, path+nameFile+'.proccess')
+                            shutil.move(path+nameFile+'.proccess', path+"Procesado/"+nameFile+'.proccess')
+                        except:
+                            print('Failed move file or rename file')
+            except:
+                print('Falla construyendo el JSON para la bd')
+            
                     
-                    try:
-                        for i, line in enumerate(file):
-                            data = {}
-                            data_split = line.split('|')  # Flat file line size
-                            if i == 0:
-                                cabecera = line.split('|')
-
-                            if i > 0:
-                                if len(data_split) > 0:
-                                    data['Flag'] = '0'
-                                    for x in range(len(cabecera)):
-                                        data[cabecera[x].rstrip('\n')] = data_split[x].rstrip('\n')
-
-                                collection.insert_one({"Json": f'{data}'})
-                                
-                    except ConnectionFailure:
-                        print('Cannot connect to Momgo DB')
-                    file.close()
-                    print('Archivo procesado: '+nameFile)
-                    try:
-                        os.rename(path+nameFile, path+nameFile+'.proccess')
-                        shutil.move(path+nameFile+'.proccess', path+"Procesado/"+nameFile+'.proccess')
-                    except:
-                        print('Failed move file or rename file')
-                    
+        try:
+            #Asignamos el job que se ejecutará en un día a una hora especifica
+            sched.add_job(timed_job, 'cron', day_of_week=source['day'], hour=source['hour'], minute=source['minute'])
+            sched.start()
+        except:
+            print('Error, debes validar las propiedades del tiempo de ejecución')
         
-        #Asignamos el job que se ejecutará en un día a una hora especifica
-        sched.add_job(timed_job, 'cron', day_of_week=source['day'], hour=source['hour'], minute=source['minute'])
-        sched.start()
 
     
